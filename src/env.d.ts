6253206294e1a904b35eb98ff8cd4b31d7ed863c
Piotr Pluzka <piotr.pluzka@bpsi.pl>
2025-11-02 13:25:34 +0100

    chore: minor fixes on layouts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,19 +1,5 @@
 /// <reference types="astro/client" />
 
-declare global {
-  namespace App {
-    interface Locals {
-      supabase: import("./db/supabase.client.ts").SupabaseClient;
-      current_user_id: string | null;
-      user: {
-        id: string;
-        email: string;
-        emailVerified: boolean;
-      } | null;
-    }
-  }
-}
-
 interface ImportMetaEnv {
   readonly SUPABASE_URL: string;
   readonly SUPABASE_KEY: string;
@@ -21,4 +7,16 @@ interface ImportMetaEnv {
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
+}
+
+declare namespace App {
+  interface Locals {
+    supabase: import("./db/supabase.client.ts").SupabaseClient;
+    current_user_id: string | null;
+    user: {
+      id: string;
+      email: string;
+      emailVerified: boolean;
+    } | null;
+  }
 }
